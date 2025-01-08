f6f0bcc35f06bcc0dc772f833a9268f45851d817
Shakti Bhakar <shakti.bhakar@hackney.gov.uk>
2025-01-08 15:45:18 +0000

    new page created with back button

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Home"; // Import Home component
-import PostDetails from "./PostDetails"; // Import PostDetails component
+import PostDetails from "./assets/PostDetails"; // Import PostDetails component
 
 const App = () => {
   return (
--- src/main.jsx
+++ src/main.jsx
@@ -6,13 +6,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route 
-      </Routes>
-    </BrowserRouter>
-
+    <App />
 
   </StrictMode>,
 )
